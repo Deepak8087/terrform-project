319525af96b975c0f104f5bfb2218b314bb325c2
Deepak8087 <Chavandeepak2011@gmail.com>
2024-10-21 18:12:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-02801556a781a4499"  # Ensure this AMI exists in the specified region
   instance_type = "t2.micro"
+  vpc_security_group_ids=["sg-0df032197bca17349"]
 
   tags = {
     Name = "jenkins-pipeline"
