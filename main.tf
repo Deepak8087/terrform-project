9811feffea64f78b0331803ef7c3b26c89178993
Deepak8087 <Chavandeepak2011@gmail.com>
2024-10-21 20:12:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.72.1"
-    }
-  }
-}
-
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-southeast-2" 
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-02801556a781a4499"  # Ensure this AMI exists in the specified region
+  ami           = "ami-0f71013b2c8bd2c29"
   instance_type = "t2.micro"
   tags = {
     Name = "jenkins-pipeline"
