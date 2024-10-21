c5f33d3bc41f1f066022c59703cc45ba3e21a8c7
Deepak8087 <Chavandeepak2011@gmail.com>
2024-10-21 17:44:55 +0530

    Update main.tf

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,12 @@
 provider "aws" {
-region = "us-east-1"
+  region = "ap-southeast-2" 
 }
 
-resource "aws_instance" "one" {
-ami = "ami-06b21ccaeff8cd686"
-instance_type = "t2.micro"
+resource "aws_instance" "example" {
+  ami           = "ami-0f71013b2c8bd2c29"
+  instance_type = "t2.micro"
+  vpc_security_group_ids=["sg-0d2c5800c5a037018"]
+  tags = {
+    Name = "jenkins-pipeline"
+  }
 }
